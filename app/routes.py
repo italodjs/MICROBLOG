d449da6af95626cb95b9d26bec9fe89894d28bc9
Italo Souza <italodjs@live.com>
2022-02-05 17:33:41 -0300

    first commit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
+@app.route('/', defaults={"nome":"home"})
 @app.route('/index', defaults={"nome":"usuário"})
 @app.route('/index/<nome>')
 def index(nome):
